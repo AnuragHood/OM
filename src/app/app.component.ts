8f0308f3726b6ec1758cbd4491a55490a0553846
AnuragHood <pathak.sn.anurag@gmail.com>
2020-02-16 00:43:43 +0530

    Anurag Pathak-grid implemented still need to refractor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'OM, Your online spiritual caretaker';
+  breakpoint: number;
+  ngOnInit() {
+    this.breakpoint = (window.innerWidth <= 479) ? 1 : 3;
+
+}
+
+onResize(event) {
+  this.breakpoint = (event.target.innerWidth <= 479) ? 1 : 3;
+  this.breakpoint = (event.target.innerWidth >= 480 && event.target.innerWidth <= 767) ? 2 : 3;
+
+}
 }
